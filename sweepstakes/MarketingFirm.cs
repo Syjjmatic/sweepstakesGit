6697c83ef868665ce621688e74dba67839eb11d8
Andrew Curzon <andrew.w.curzon@gmail.com>
2020-03-02 10:01:32 -0600

    Added MarketingFirm class and laid groundwork methods for it.

---
+++ sweepstakes/MarketingFirm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sweepstakes
+{
+    class MarketingFirm
+    {
+        ISweepstakesManager _manager;
+
+        public MarketingFirm(ISweepstakesManager manager)
+        {
+
+        }
+
+        public void CreateSweepstake()
+        {
+
+        }
+    }
+}
