b3ad30f48354a17d8c623c1fbe96dbdf67dfee18
Andrew Curzon <andrew.w.curzon@gmail.com>
2020-03-02 07:36:07 -0600

    Added Contestant class and gave it several variables.

---
+++ sweepstakes/Contestant.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sweepstakes
+{
+    public class Contestant
+    {
+        public string firstName;
+        public string lastName;
+        public string emailAddress;
+        public int registrationNumber;
+
+        public Contestant()
+        {
+
+        }
+    }
+}
