8495de935bcf99944743479013aa8af88fed81a8
Andrew Curzon <andrew.w.curzon@gmail.com>
2020-03-02 09:57:55 -0600

    Created SweepstakesQueueManager class and laid groundwork methods for it.

---
+++ sweepstakes/SweepstakesQueueManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sweepstakes
+{
+    class SweepstakesQueueManager
+    {
+        Queue<Sweepstakes> queue;
+        public SweepstakesQueueManager()
+        {
+
+        }
+
+        public void InsertSweepstakes(Sweepstakes sweepstakes)
+        {
+
+        }
+
+        public Sweepstakes GetSweepstakes()
+        {
+            string sweepstakesStr = "sweepstakes";
+            Sweepstakes sweepstakes = new Sweepstakes(sweepstakesStr);
+            return sweepstakes;
+        }
+    }
+}
