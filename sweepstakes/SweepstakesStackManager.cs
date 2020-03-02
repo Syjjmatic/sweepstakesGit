7dfa6570e6d63a5faa186b430594a270e102e58e
Andrew Curzon <andrew.w.curzon@gmail.com>
2020-03-02 09:54:21 -0600

    Created SweepstakesStackManager class and laid groundwork methods for it.

---
+++ sweepstakes/SweepstakesStackManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sweepstakes
+{
+    class SweepstakesStackManager
+    {
+        Stack<Sweepstakes> stack;
+        public SweepstakesStackManager()
+        {
+
+        }
+
+        public void InsertSweepstakes(Sweepstakes sweepstakes)
+        {
+
+        }
+
+        public Sweepstakes GetSweepstakes()
+        {
+            string sweepstakesStr = "sweepstakes";
+            Sweepstakes sweepstakes = new Sweepstakes(sweepstakesStr);
+            return sweepstakes;
+        }
+    }
+}
